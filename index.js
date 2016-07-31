cbc430b8c622cd2323db524b3681e812f846bde0
Teacher <Teacher>
2016-07-30 20:35:10 -0400

    added daily and minutely routes

--- index.js
+++ index.js
@@ -33,6 +33,57 @@ server.get('/forecast/hourly/:lat,:lon', function(req, res){
       });
 });
 
+server.get('/forecast/minutely/:lat,:lon', function(req, res){
+  $http.get(baseUrl + apiKey + '/'+req.params.lat+','+req.params.lon)
+        .then(function(response){
+          var resObj = {
+            latitude: response.data.latitude,
+            longitude: response.data.longitude,
+            minutely: response.data.minutely
+          };
+
+          res.status(200).json(resObj);
+        })
+        .catch(function(error){
+          res.status(500).json({
+            msg: error
+          });
+        });
+});
+
+server.get('/forecast/daily/:lat,:lon', function(req, res){
+  $http.get(baseUrl + apiKey + '/' + req.params.lat + ',' + req.params.lon)
+      .then(function(response){
+        var overSummary = response.data.daily.summary;
+        var overIcon = response.data.daily.icon;
+        var dailyData = response.data.daily.data;
+        var dailyArr = [];
+        for(var i = 0; i < dailyData.length; i += 1){
+          var o = {
+            icon: dailyData[i].icon,
+            tempMax: dailyData[i].temperatureMax,
+            tempMin: dailyData[i].temperatureMin,
+            humidity: dailyData[i].humidity,
+            precipProb: dailyData[i].precipProbability
+          };
+          dailyArr.push(o);
+        }
+
+        var resObj = {
+          latitude: response.data.latitude,
+          longitude: response.data.longitude,
+          summary: overSummary,
+          icon: overIcon,
+          daily: dailyArr
+        };
+        res.status(200).json(resObj);
+      })
+      .catch(function(err){
+        res.status(500).json({
+          msg: err
+        });
+      });
+
 //listen
 
 server.listen(port, function(){
